5143d5d71d39140df78088b5d6e62484a575e593
Pratanu Roy <roy23@llnl.gov>
2017-07-31 12:42:11 -0700

    Added discussion comments in the beginning of the main file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
-/*
- * main.cpp
- *
- *  Created on: Jul 30, 2017
- *      Author: Pratanu Roy
- */
+//=========================================================================================================
+//*********************************************************************************************************
+//
+//  Purpose:
+//
+//    MAIN is the main program for polygon_in_point_test
+//
+//  Discussion:
+//
+// 	A rectangular two-dimensional domain is populated by a Cartesian mesh with identical spacing in the X
+//		and Y directions. The left bottom corner of the domain is located at (0.0, 0.0). An arbitrary shaped
+//		polygon described as a sequence of vertices ordered in anti-clockwise direction is overlaid on this
+//		Cartesian mesh. The polygon is completely contained in the rectangular domain. This program find the
+//    location of the points with respect to the polygon and mark the cells of the Cartesian mesh with a 
+//    location attribute described in the following manner.
+//		- Completely inside the polygon: I
+//		- Intersecting the polygon segments: X
+//		- Completely outside the polygon: O
+//
+//  Modified:
+//
+//    July 31, 2017
+//
+//  Author:
+//
+//    Pratanu Roy
+//
+//*********************************************************************************************************
+//=========================================================================================================
 
 #include <iostream>
 #include <vector>
